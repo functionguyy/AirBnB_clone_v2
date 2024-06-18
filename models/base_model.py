245f7a1d12468887b01fdd16b94187dee71ce738
Gilbert Adikankwu <gilbertadikankwu@gmail.com>
2024-06-18 11:45:02 +0100

    feat: delete current instance from storage

--- models/base_model.py
+++ models/base_model.py
@@ -56,3 +56,8 @@ class BaseModel:
         if '_sa_instance_state' in dictionary.keys():
             del dictionary['_sa_instance_state']
         return dictionary
+
+    def delete(self):
+        """delete the current instance from the storage"""
+        from models import storage
+        storage.delete(self)
