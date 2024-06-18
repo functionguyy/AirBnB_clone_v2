f3f07a8b645734c84346a8ad3248ea719622ff1d
Gilbert Adikankwu <gilbertadikankwu@gmail.com>
2024-06-18 14:28:56 +0100

    feat: map city class to cities table

--- models/city.py
+++ models/city.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 
 
-class City(BaseModel):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+class City(BaseModeli, Base):
+    """The city class, contains state ID and name
+    """
+    __tablename__ = 'cities'
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), nullable=False, ForeignKey("states.id"))
