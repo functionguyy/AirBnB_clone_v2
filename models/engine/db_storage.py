f24f45885c91679642ba7d3e2dcdcd0bafb2d7ae
Gilbert Adikankwu <gilbertadikankwu@gmail.com>
2024-06-23 21:08:32 +0100

    fix: add methods new(), save(), reload() and delete()

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,18 +1,18 @@
-#!/usr/bin/python3
+#!/home/functionguyy/myenv/bin/python3
 """This module defines a class to manage database storage for hbnb clone"""
 from sqlalchemy import create_engine
 from os import getenv
 from models.base_model import Base
-from sqlalchemy import sessionmaker
+from sqlalchemy.orm import sessionmaker
 from models.user import User
 from models.place import Place
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.review import Review
+from sqlalchemy.orm import scoped_session
 
 
-Session = sessiomaker()
 
 class DBStorage:
     """engine for database storage of models"""
@@ -31,26 +31,51 @@ class DBStorage:
         PASS = getenv('HBNB_MYSQL_PWD')
         HOST = getenv('HBNB_MYSQL_HOST')
         DB = getenv('HBNB_MYSQL_DB')
-        db_url = f"mysql+mysqldb://{USER}:{PASS}@localhost/{DB}"
-        DBStorage.__engine = create_engine(db_url, pool_pre_ping=True)
+        db_url = f"mysql+mysqldb://{USER}:{PASS}@{HOST}/{DB}"
+        self.__engine = create_engine(db_url, pool_pre_ping=True,
+                echo=True)
 
         if getenv('HBNB_ENV') == 'test':
             Base.metadata.drop_all()
 
     def all(self, cls=None):
-        Session.configure(bind=DBStorage.__engine)
-        DBStorage.__session = Session()
-
+        """ query on the current database session all objects depending on the
+        class name"""
         query_list = []
         obj_dict = {}
 
-        if cls:
-            query_list.extend(DBStorage.__session.query(cls).all())
+        if cls is not None:
+            if isinstance(cls, str):
+                cls = self.classes.get(cls)
+            print(f"Mapped class: {cls}")
+            query_list.extend(self.__session.query(cls).all())
         else:
-            for cls in DBStorage.classes.values():
-                query_list.extend(DBStorage.__session.query(cls).all())
+            for cls in self.classes.values():
+                query_list.extend(self.__session.query(cls).all())
 
         for obj in query_list:
             obj_dict[obj.__class__.__name__ + '.' + obj.id] = obj
 
         return obj_dict
+
+
+    def new(self, obj):
+        """add the object to current database session"""
+        self.__session.add(obj)
+
+    def save(self):
+        """commit all changes of the current database session"""
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """ delete from the current database session"""
+
+        if obj:
+            self.__session.delete(obj)
+
+    def reload(self):
+        """ create all tables in the database"""
+        Base.metadata.create_all(self.__engine)
+        sess = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(sess)
+        self.__session = Session()
