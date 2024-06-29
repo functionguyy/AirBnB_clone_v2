40d3a57b6e61105e224e48333dfddfe24494d041
Gilbert Adikankwu <gilbertadikankwu@gmail.com>
2024-06-29 15:45:06 +0100

    fix: make str() return dic with _sa_instance_state key

--- models/base_model.py
+++ models/base_model.py
@@ -35,7 +35,10 @@ class BaseModel:
     def __str__(self):
         """Returns a string representation of the instance"""
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+        dic = self.__dict__.copy()
+        if '_sa_instance_state' in dic.keys():
+            del dic['_sa_instance_state']
+        return '[{}] ({}) {}'.format(cls, self.id, dic)
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
